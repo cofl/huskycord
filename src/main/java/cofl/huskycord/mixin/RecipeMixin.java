3de8fe3044c1b6711e4f2062d9d30e4aecb0f126
Christian LaCourt <cllacour@mtu.edu>
2024-06-19 17:00:19 -0400

    Grave logic is evil but functional probably

--- src/main/java/cofl/huskycord/mixin/RecipeMixin.java
+++ src/main/java/cofl/huskycord/mixin/RecipeMixin.java
@@ -14,14 +14,10 @@ public abstract class RecipeMixin implements CraftingRecipe {
     @Override
     @SuppressWarnings("ConstantConditions")
     public @NotNull NonNullList<ItemStack> getRemainingItems(CraftingInput input){
-        LOGGER.info("In method.");
         var list = NonNullList.withSize(input.size(), ItemStack.EMPTY);
-        var allowRetain = this instanceof CraftingRecipe;
-
         for(var i = 0; i < list.size(); i += 1){
             var stack = input.getItem(i);
             var item = stack.getItem();
-            LOGGER.info("Checking remaining items for {}: allow? {}, keep? {}", stack, allowRetain, stack.is(HuskycordMod.RETAIN_IN_CRAFTING));
 
             if(item.hasCraftingRemainingItem()){
                 list.set(i, new ItemStack(item.getCraftingRemainingItem()));
